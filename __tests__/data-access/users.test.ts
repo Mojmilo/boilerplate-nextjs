5b4e5be4b0eac5f984e549f5749f90c2203f0c35
MaxVzl <maxime.vozelle@gmail.com>
2024-10-27 18:00:12 +0100

    commit

--- __tests__/data-access/users.test.ts
+++
@@ -1,9 +0,0 @@
-import {createUser} from "@/data-access/users";
-
-describe('Users', () => {
-    it('create user', async () => {
-        expect(await createUser({
-            email: 'test@test.com',
-        }));
-    });
-});
